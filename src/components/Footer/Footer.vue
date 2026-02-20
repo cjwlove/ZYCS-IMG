8def6549b22d02ed17c6442a24efdd1af3374c47
cjwlove <114965351+cjwlove@users.noreply.github.com>
2026-02-20 13:15:48 +0800

    Update Footer.vue

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -30,7 +30,7 @@
           <a title="Github" target="_blank" rel="nofollow" href="https://github.com/uxiaohan">
             <img class="w-8" src="@/assets/images/footer/github.svg" />
           </a>
-          <a title="韩小韩博客" target="_blank" href="https://www.vvhan.com/">
+          <a title="kuzhan.cc" target="_blank" href="https://kuzhan.cc/">
             <img class="w-8" src="@/assets/images/footer/blog.svg" />
           </a>
         </div>
